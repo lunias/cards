0f6587a009203d64aba45012d346d57a0087480c
Ethan Anderson <ethanaa@gmail.com>
2015-02-06 15:51:41 -0600

    Working on getting sorting working w/ infinite scroll

--- oauth-server/src/main/webapp/scripts/app/main/main.controller.js
+++ oauth-server/src/main/webapp/scripts/app/main/main.controller.js
@@ -12,6 +12,8 @@ angular.module('cardsOauthApp').controller('MainController',
 
 			var totalPages = 0;
 			var currentPage = 0;
+			var sortField = '';
+			var sortDir = '';
 			
 			$scope.userGridOptions = {
 				infiniteScrollPercentage : 15,
@@ -29,6 +31,7 @@ angular.module('cardsOauthApp').controller('MainController',
 					field : 'langKey'
 				} ],
 				onRegisterApi : function(gridApi) {
+					
 					gridApi.infiniteScroll.on.needLoadMoreData($scope, function() {
 						
 						if (currentPage < totalPages - 1) {
@@ -37,39 +40,66 @@ angular.module('cardsOauthApp').controller('MainController',
 							return;
 						}
 						
-						updateUserGrid(function(data) {
-							console.log(data);
+						updateUserGrid(false, function(data) {
 							gridApi.infiniteScroll.dataLoaded();
 							
 						}, function(err) {
-							console.log(err);
 							gridApi.infiniteScroll.dataLoaded();
 						});
+					});
+					
+					gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
+						
+						// TODO support multi column sort
+						sortField = sortColumns.length ? sortColumns[0].field : '';
+						sortDir = sortColumns.length ? sortColumns[0].sort.direction : '';
+						currentPage = 0;
+							
+						// scroll to top
+						gridApi.cellNav
+						.scrollTo(
+								grid,
+								$scope,
+								$scope.userGridOptions.data[0],
+								$scope.userGridOptions.columnDefs[0]);						
+						
+						updateUserGrid(true);												
+						
+						grid.options.loadTimout = false;
 					});
 				} 
 			}
 			
-			function updateUserGrid(success, err) {
+			function updateUserGrid(replaceData, success, err) {
 				
+				var replaceData = replaceData || false;
 				var success = success || angular.noop;
 				var error = err || angular.noop;
 				
-				User.findAll({page: currentPage}, function(usersResponse) {
+				var params = {
+						page: currentPage
+				};
+				
+				if (sortField) params.sort = sortField + ',' + sortDir;
+				
+				User.findAll(params, function(usersResponse) {
 				
 					currentPage = usersResponse.page.number;
 					totalPages = usersResponse.page.totalPages;					
 					
-					$scope.userGridOptions.data = usersResponse.body.reduce(function(coll, item) {
-						coll.push(item);
-						return coll;
-					}, $scope.userGridOptions.data);					
+					if (replaceData) {
+						$scope.userGridOptions.data = usersResponse.body;						
+					} else {						
+						$scope.userGridOptions.data = usersResponse.body.reduce(function(coll, item) {
+							coll.push(item);
+							return coll;
+						}, $scope.userGridOptions.data);
+					}
 					
 					success(usersResponse);
 					
-				}, function(err) {
-					
-					error(err);
-					
+				}, function(err) {					
+					error(err);					
 				});								
 			}
 			
